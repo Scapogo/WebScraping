acc17c8bbcf78faf4e8f212f3e3ba48fd83ebc98
Scapogo <scapogo@gmail.com>
2016-12-21 12:03:13 -0800

    first pages with mongodb connection

--- RealEstateSite/estates/urls.py
+++ RealEstateSite/estates/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^senica/$', views.senica, name='senica'),
+    url(r'^senica/(?P<room_number>\d+)/$', views.senica_room_filter, name='senica_room_filter'),
 ]
--- RealEstateSite/estates/views.py
+++ RealEstateSite/estates/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import pymongo
+
+connection = pymongo.MongoClient('localhost', 27017)
+db = connection.RealEstate
+adverts_db = db.Adverts
 
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the estates index.")
-    return  render(request, 'estates/index.html')
+    return render(request, 'estates/index.html')
+
+
+def senica(request):
+
+    adverts = adverts_db.find({'City': 'Senica'}, {'_id': 0, 'Id': 1, 'Price': 1, 'Location': 1,
+                                               'NumberOfRooms': 1, 'Age': 1, 'LivingAreaM2': 1})
+
+    count = adverts.count()
+    total = 0
+
+    for advert in adverts:
+        total += int(advert['Price'][0])
+
+    average = total/count
+
+    context = {'average': average, 'count': count}
+    return render(request, 'estates/senica.html', context)
+
+
+def senica_room_filter(request, room_number):
+    adverts = adverts_db.find({'City': 'Senica', 'NumberOfRooms': int(room_number)}, {'_id': 0, 'Id': 1, 'Price': 1,
+                                            'Location': 1, 'NumberOfRooms': 1, 'Age': 1, 'LivingAreaM2': 1})
+
+    count = adverts.count()
+    total = 0
+
+    if count > 0:
+        for advert in adverts:
+            total += int(advert['Price'][0])
+
+        average = total / count
+    else:
+        average = 0
+
+    context = {'average': average, 'count': count}
+    return render(request, 'estates/senica.html', context)
