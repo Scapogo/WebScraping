c75e258278355a1e884c888edc57b9c29fd8b210
Scapogo <scapogo@gmail.com>
2017-01-09 12:43:55 -0800

    first version of stat gathering script

--- ProcessItems/processitems.py
+++ ProcessItems/processitems.py
@@ -1,6 +1,7 @@
 import pymongo
 import sys
 import datetime
+import numpy
 
 sys.path.append("..")
 
@@ -16,10 +17,57 @@ db = client['real_estate']
 adv_coll = db['Adverts']
 stats_coll = db['Stats_data']
 
+
+def getlist(adverts):
+    pricelist = []
+
+    for advert in adverts:
+        pricelist.append(int(advert['Price'][0]))
+
+    return pricelist
+
+
 for city in config.CITY_LIST:
     stats_coll.insert({'City': city[1], 'Date': date})
 
     for category in config.TYPE_CHOICES:
+        if category[0] == 1:
+            adverts = adv_coll.find({'City': city[1], 'NumberOfRooms': category[0], 'House': 0}, {'Price': 1})
+
+            pricelist = getlist(adverts)
+
+            avg = sum(pricelist)/len(pricelist)
+            min_price = min(pricelist)
+            max_price = max(pricelist)
+            med = numpy.median(pricelist)
+
+            stats_coll.update({'City': city[1], 'Date': date}, {'set': {
+                'Stats': [{'category': category[0], 'Avg': avg, 'Min': min_price, 'Max': max_price, 'Med': med}]}})
+        elif category[0] < 5:
+            adverts = adv_coll.find({'City': city[1], 'NumberOfRooms': category[0], 'House': 0}, {'Price': 1})
+
+            pricelist = getlist(adverts)
+
+            avg = sum(pricelist) / len(pricelist)
+            min_price = min(pricelist)
+            max_price = max(pricelist)
+            med = numpy.median(pricelist)
+
+            stats_coll.update({'City': city[1], 'Date': date}, {'$push': {
+                'Stats': [{'category': category[0], 'Avg': avg, 'Min': min_price, 'Max': max_price, 'Med': med}]}})
+        elif category[0] >= 5:
+            adverts = adv_coll.find({'City': city[1], 'NumberOfRooms': category[0], 'House': 0}, {'Price': 1})
+
+            pricelist = getlist(adverts)
+
+            avg = sum(pricelist) / len(pricelist)
+            min_price = min(pricelist)
+            max_price = max(pricelist)
+            med = numpy.median(pricelist)
+
+            stats_coll.update({'City': city[1], 'Date': date}, {'$push': {
+                'Stats': [{'category': category[0], 'Avg': avg, 'Min': min_price, 'Max': max_price, 'Med': med}]}})
+
         # TODO go through all types and get statistics about those adverts into stats collection
 
 # 1. Create new document: {'City': city, 'Date': date}
